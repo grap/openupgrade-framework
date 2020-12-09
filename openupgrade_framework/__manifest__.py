c4ffd769fdfd2aa1f06ead6fca2b77ecca876859
Stefan Rijnhart <stefan@opener.am>
2020-12-09 22:20:32 +0100

    [IMP] Auto upgrade-path; [RFR] Readme

--- openupgrade_framework/__manifest__.py
+++ openupgrade_framework/__manifest__.py
@@ -4,8 +4,7 @@
     "name": "Openupgrade Framework",
     "summary": """Module to integrate in the server_wide_modules
     option to make upgrades between two major revisions.""",
-    "author": "Odoo SA, Odoo Community Association (OCA),"
-    " Therp BV, Opener B.V., GRAP",
+    "author": "Odoo Community Association (OCA)," " Therp BV, Opener B.V., GRAP",
     "website": "https://github.com/OCA/openupgrade-framework",
     "category": "Migration",
     "version": "14.0.1.0.0",
